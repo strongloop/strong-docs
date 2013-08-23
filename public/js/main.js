14485985385800fe90f153a20c143dc087b02482
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2013-08-23 13:43:31 -0700

    Add new CSS and scrollspy support.

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
+$(document).ready(function () {
+  // Switch from empty anchors to id-ed headings
+  $('a[name]').get().forEach(function (i) {
+    var $i = $(i);
 
+    $i.next().attr('id', $i.attr('name'));
+    $i.detach();
+  });
+});
