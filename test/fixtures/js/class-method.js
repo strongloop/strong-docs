d50ff17f5557b959c1372dbd5e1399216fb27b2d
Hage Yaapa <captain@hacksparrow.com>
2016-06-25 01:41:42 +0530

    Add ability to include methods from independent files
    The methods of a classs need not be defined in the same file as
    the class file. They can be included from independent files by
    adding `/* @class <Name of the class> */` at the top of the
    file to be included.

---
+++ test/fixtures/js/class-method.js
@@ -0,0 +1,13 @@
+/* @class LoopBackApplication */
+
+/*! */
+var proto = {};
+
+/**
+ * Register a middleware handler to be executed in a given phase.
+ *
+ * @header app.middleware(name, handler)
+ */
+proto.middleware = function(name, paths, handler) {
+
+}
