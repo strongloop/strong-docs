87e5d4f4a69e8c9dad706a8510e8a1cd3534ca8f
Mahesh Patsute <mahesh_patsute@yahoo.com>
2017-06-22 14:16:50 -0700

    Type script (#84)
    * Refactored
    
    markdown, code highlighting support, and code example support
    
    * Added null check
    
    * fixed exportedConstructs processing
    
    * added text
    
    * Added returns text
    
    * Fixed two headers
    
    * Added shortText for args
    
    * Added comment and return text
    
    * fixed broken test
    
    * Fixed file filtering
    
    * fixed method rendering
    
    * Added construct Type
    
    * fixed rendering
    
    * added travis yml
    
    * fixed anchor for Type aliases
    
    * Added comments for Type aliases

--- test/ts-parser.test.js
+++ test/ts-parser.test.js
@@ -8,7 +8,9 @@ describe('TypeScript Parser Test', function() {
 
   it('should parse this TS file', function(done) {
     var file = path.join(__dirname, 'fixtures' , 'ts', 'Greeter.ts');
-    var tsParser = new TSParser(file);
+    var tsFiles = [];
+    tsFiles.push(file);
+    var tsParser = new TSParser(tsFiles);
     var parsedData = tsParser.parse();
     assert.equal(parsedData.sections.length, 3);
     assert.equal(parsedData.constructs.length, 1);
