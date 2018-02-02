9a66707505edf7cf84008115cb650d3e2c073e0b
Raymond Feng <enjoyjava@gmail.com>
2018-02-01 20:36:44 -0800

    fix: set excludeNotExported to false to work around typedoc issue
    See https://github.com/TypeStrong/typedoc/pull/694

--- test/ts-parser.test.js
+++ test/ts-parser.test.js
@@ -5,16 +5,31 @@ var expect = require('chai').expect;
 var TSParser = require('../lib/tsParser');
 
 describe('TypeScript Parser Test', function() {
-  var tsOptions = {
-    excludeNotExported: false,
-    tsconfig: path.join(__dirname, 'tsconfig.json'),
-  };
+  var tsconfig = path.join(__dirname, 'tsconfig.json');
 
-  it('should parse this TS file', function() {
+  it('should parse TS file', function() {
     var file = path.join(__dirname, 'fixtures/ts/Greeter.ts');
     var tsFiles = [file];
-    var tsParser = new TSParser(tsFiles, tsOptions);
+    var tsParser = new TSParser(tsFiles, {
+      excludeNotExported: false,
+      tsconfig,
+    });
     var parsedData = tsParser.parse();
+    expect(parsedData.sections).to.have.length(3);
+    expect(parsedData.constructs).to.have.length(1);
+    expect(parsedData.errors).to.have.length(0);
+  });
+
+  it.skip('should parse TS file for exported ' +
+    '(https://github.com/TypeStrong/typedoc/pull/694)', function() {
+    var file = path.join(__dirname, 'fixtures/ts/Greeter.ts');
+    var tsFiles = [file];
+    var tsParser = new TSParser(tsFiles, {
+      excludeNotExported: true,
+      tsconfig,
+    });
+    var parsedData = tsParser.parse();
+    console.log(parsedData);
     expect(parsedData.sections).to.have.length(3);
     expect(parsedData.constructs).to.have.length(1);
     expect(parsedData.errors).to.have.length(0);
@@ -23,7 +38,10 @@ describe('TypeScript Parser Test', function() {
   it('should report errors based on tsconfig', function() {
     var file = path.join(__dirname, 'fixtures/ts/Greeter.es2016.ts');
     var tsFiles = [file];
-    var tsParser = new TSParser(tsFiles, tsOptions);
+    var tsParser = new TSParser(tsFiles, {
+      excludeNotExported: false,
+      tsconfig,
+    });
     var parsedData = tsParser.parse();
     expect(parsedData.errors).to.have.length(1);
     expect(parsedData.errors[0].messageText).to.eql(
