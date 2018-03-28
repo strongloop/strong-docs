0e79a3002bf8708ddd0fdca0f1a5f24d928475d9
Raymond Feng <enjoyjava@gmail.com>
2018-03-28 14:44:44 -0700

    fix: update templates for methods and type aliases

--- test/fixtures/ts/Greeter.ts
+++ test/fixtures/ts/Greeter.ts
@@ -1,22 +1,25 @@
 /** Class representing a greeter. */
 export class Greeter {
-    greeting: string;
-    greeting2: number;
-    /**
-     * constructor comments
-     */
-    constructor(message: string) {
-        this.greeting = message;
-    }
-    greet() {
-        return "Hello, " + this.greeting;
-    }
+  greeting: string;
+  greeting2: number;
+  /**
+   * constructor comments
+   */
+  constructor(message: string) {
+    this.greeting = message;
+  }
+  greet(): string;
+  greet(prefix: string): string;
+
+  greet(prefix?: string) {
+    prefix = prefix || '';
+    return `[${prefix}] Hello, ${this.greeting}`;
+  }
 }
 
-function greeterFun(age: number){
-}
+function greeterFun(age: number) {}
 
-let greeter = new Greeter("world");
+let greeter = new Greeter('world');
 
 export type PathParameterValues = {[key: string]: any};
 
@@ -36,5 +39,5 @@ export namespace param {
   export const path = {
     number: function() {},
     boolean: function() {},
-  }
+  };
 }
